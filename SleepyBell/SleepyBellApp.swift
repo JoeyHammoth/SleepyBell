1402a8efb42be5a397afea3b134a59277689efb0
JoeyHammoth <137293223+JoeyHammoth@users.noreply.github.com>
2025-01-31 20:25:05 +0700

    Initial Commit

---
+++ SleepyBell/SleepyBellApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepyBellApp.swift
+//  SleepyBell
+//
+//  Created by James Nikolas on 1/31/25.
+//
+
+import SwiftUI
+
+@main
+struct SleepyBellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
